796533f50c6964ec86a013d91c7de0f9b4135897
Vedant Dhruv <vdhruv2@illinois.edu>
2024-03-27 12:12:13 -0500

    run all tests script. And so begins the automation process for convergence tests

---
+++ tests/convergence/run_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# One script to rule them all.
+# A single bash script to run all convergence tests. If you'd instead like to
+# run specific convergence tests, run the bash script corresponding to that
+# test, e.g., test_mhdmodes1d.sh
+
+./test_mhdmodes1d.sh
+./test_mhdmodes2d.sh
